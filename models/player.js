3c50e636a153f8c7373f8adf48443e56baa58958
Erlemorgaine <erle_3@hotmail.com>
2018-02-28 15:04:05 +0100

    messages added to api

--- models/player.js
+++ models/player.js
@@ -5,14 +5,20 @@ const villageSchema = new Schema({
   name: { type: String },
 });
 
+const messageSchema = new Schema({
+  name: { type: String },
+  message: { type: String },
+  senderName: { type: String }
+});
 
 const playerSchema = new Schema({
   village: [villageSchema],
   name: {type: String },
   mayor: { type: Boolean, default: false },
   dead: { type: Boolean, default: false },
-  message: { type: Boolean, default: false },
+  messageSent: { type: Boolean, default: false },
   photo: { type: String },
+  receivedMessages: { type: Array, default: [] }
 });
 
 module.exports = mongoose.model('players', playerSchema)
